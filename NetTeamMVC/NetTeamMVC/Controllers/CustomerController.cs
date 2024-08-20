328e889dbf2aa718711c6d0a8db3466ca9c3bded
mfuentes85 <mfuentes85@gmail.com>
2024-08-19 19:07:44 -0600

    MVC Video 3 no terminado

---
+++ NetTeamMVC/NetTeamMVC/Controllers/CustomerController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetTeamMVC.Models;
+using System.Reflection;
+
+namespace NetTeamMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static List<CustomerModel> Customers = new List<CustomerModel>()
+        {
+        new CustomerModel()
+        {
+                Id=1,
+                Name = "Israel Perez Trejo",
+                Address="Av Siempre Viva 123",
+
+        },
+
+        new CustomerModel
+        {
+            Id = 2,
+            Name = "Emanuel Durán",
+            Address = "Venezuela"
+
+        },
+
+
+        new CustomerModel
+        {
+            Id = 3,
+            Name = "Claudia Durán",
+            Address = "Venezuela, Ve"
+
+        },
+     };
+
+       public CustomerController()
+        {
+            //Customers.Add(new CustomerModel
+            //{
+            //    Id = 123,
+            //    Name = "Israel Perez ",
+            //    Address = "Av Siempre Viva 123",
+
+            //});
+
+            //Customers.Add(new CustomerModel
+            //{
+            //    Id = 234,
+            //    Name = "Emanuel Durán",
+            //    Address = "Venzuela",
+
+            //});
+
+            //Customers.Add(new CustomerModel
+            //{
+            //    Id = 345,
+            //    Name = "Claudia Duran",
+            //    Address = "Caracas, Ve",
+
+            //});
+        }
+
+
+      
+
+        public IActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        public IActionResult Creation()
+        {
+            return View(new NetTeamMVC.Models.CustomerModel
+            {
+                Name= "Isra Pérez",
+                Address= "Ecatepec, México",
+
+
+            });
+        }
+
+        public IActionResult Save(CustomerModel customer)
+        {
+            Customers.Add(customer);
+            return View("Index");
+        }
+    }
+
+
+
+
+    //public IActionResult Details()
+    //{
+    //    CustomerModel model = new CustomerModel
+    //    {
+    //        Id = 1,
+    //        Name = "Israel Pérez Trejo",
+    //        Address= "Ecatepec de Morelos, México"
+
+    //    };
+    //    return View(model);
+    //}
+}
---
+++ NetTeamMVC/NetTeamMVC/Models/CustomerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NetTeamMVC.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
